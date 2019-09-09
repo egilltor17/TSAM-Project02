e5da6b5c01746437dbecefcac4dd629b644596c0
egilltor17 <egilltor17@ru.is>
2019-09-09 18:59:43 +0000

    Pulling

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -50,8 +50,6 @@ void signalHandler(const int signum) {
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in server; 
-    char buffer[BUFFER_SIZE];
-    std::string buffer2;
     std::string host;
     sock = 0;
     int ip_low = 0;
@@ -93,14 +91,19 @@ int main(int argc, char *argv[]) {
     hostent *record = gethostbyname(argv[1]);
     host = record ? inet_ntoa(*(in_addr *)record->h_addr) : argv[1];
 
+    // Set type of connection
+    server.sin_family = AF_INET;
+
     // Scanning loop
     for(int i = ip_low; i <= ip_high; i++) {
 
-        // // Open a socket TCP
-        // if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-        //     PRINT_ERROR("Socket creation error");
-        //     return -1;
-        // }
+        /* 
+        // Open a socket TCP
+        if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+            PRINT_ERROR("Socket creation error");
+            return -1;
+        }
+        */
         
         // Open a socket UDP
         if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
@@ -108,14 +111,14 @@ int main(int argc, char *argv[]) {
             return -1;
         }
         
-        // // Open a socket RAW (might need sudo)
-        // if((sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0) { 
-        //     PRINT_ERROR("Socket creation error");
-        //     return -1;
-        // }
+        /*
+        // Open a socket RAW (might need sudo)
+        if((sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0) { 
+            PRINT_ERROR("Socket creation error");
+            return -1;
+        }
+        */
 
-        // Set type of connection
-        server.sin_family = AF_INET;
         // Set port
         server.sin_port = htons(i); 
  
@@ -130,28 +133,10 @@ int main(int argc, char *argv[]) {
             printf("Port: %d - Connection failed\n", i);
             // PRINT_ERROR("Connection Failed");
             // return -1;
-        } else {
-            printf("Port: %d - Connection succsessfull\n", i);
         }
         
-        // Main loop
-        while(!true) {
-            // Prefixes the commandline with the host:$
-            printf("\033[0;32m%s:\033[0m$ ", argv[1]);
-            
-            // Get and send the user input
-            memset(buffer, 0, sizeof(buffer));
-            std::getline(std::cin, buffer2);
-            strncpy(buffer, buffer2.c_str(), COPY_SIZE(buffer2));
-            send(sock, buffer, sizeof(buffer), 0);
-            
-            // Receive and dispaly the server responce
-            memset(buffer, 0, sizeof(buffer));
-            if(recv(sock, buffer, sizeof(buffer), 0) <= 0) {
-                printf("No reply\n");
-            } else {
-                printf("%s", buffer);
-            }
+        if(sendto(sock, "msg", sizeof("msg"), 0, (sockaddr *)&server, sizeof(server)) < 0) {
+            printf("someting happend :(\n");
         }
 
         close(sock);
